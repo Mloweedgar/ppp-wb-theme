4d7b6fc88d2086a4b073fc579964b25564dfc04c
mloweedgar <mloweedgar0@gmail.com>
2020-01-03 12:34:56 +0300

    setup theme

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Zanzibar PPP department</title>
+</head>
+<body>
+<header>This is main navigation bar</header>
+<section>This is main content</section>
+<footer>this is footer section </footer>
+</body>
+</html>
