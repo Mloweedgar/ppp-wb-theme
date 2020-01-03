9395ad7694a6573038640fd48bc909b619e4c5b8
mloweedgar <mloweedgar0@gmail.com>
2020-01-03 18:25:04 +0300

    feat: implement section with logo and flag in header

--- header.php
+++ header.php
@@ -1,44 +1,47 @@
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/html">
 <head>
     <title>Zanzibar PPP department</title>
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
-    <?php wp_head();?>
+    <?php wp_head(); ?>
 </head>
 <body>
 <div class="site-contents">
-<header class="navigation-bar">
-    <section class="top-navigation greysh-background bluish-text-color small-text">
-        <aside>December, 23, 2019</aside>
-        <aside>
-            <nav>
-                <div class="vertical-divider">
-                    <a>Discussion Forum </a>
-                </div>
-                <div class="vertical-divider">
-                    <a>News</a>
-                </div>
-                <div class="vertical-divider">
-                    <a>FAQS</a>
-                    <span class=""></span>
-                </div>
-                <div class="vertical-divider">
-                    <a>Sitemap</a>
-                </div>
-                <div class="vertical-divider">
-                    <a>Contact Us</a>
-                </div>
-            </nav>
-            <select class="language-selector">
-                <option selected="selected">Swahili</option>
-                <option>English</option>
-            </select>
-        </aside>
-    </section>
-    <section class="logos-section">
-        This is logosection
-    </section>
-    <section class="main-navigation">
-        this is the main navigation
-    </section>
-</header>
+    <header class="navigation-bar">
+        <section class="top-navigation greysh-background bluish-text-color small-text">
+            <aside>December, 23, 2019</aside>
+            <aside>
+                <nav>
+                    <div class="vertical-divider">
+                        <a>Discussion Forum </a>
+                    </div>
+                    <div class="vertical-divider">
+                        <a>News</a>
+                    </div>
+                    <div class="vertical-divider">
+                        <a>FAQS</a>
+                        <span class=""></span>
+                    </div>
+                    <div class="vertical-divider">
+                        <a>Sitemap</a>
+                    </div>
+                    <div class="vertical-divider">
+                        <a>Contact Us</a>
+                    </div>
+                </nav>
+                <select class="language-selector">
+                    <option selected="selected">Swahili</option>
+                    <option>English</option>
+                </select>
+            </aside>
+        </section>
+        <section class="logos-section">
+            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/zanzibar-coat-of-arms.jpg" alt="Zanzibar Coat of Arms" width="100" height="100">
+            <h3 class="bluish-text-color">Zanzibar Private Public Partnership <br/>(PPP) Department</h3>
+            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/flag-of-zanzibar.svg" alt="Flat of Zanzibar" width="100" height="100">
+
+        </section>
+        <section class="main-navigation">
+            this is the main navigation
+        </section>
+    </header>
