ad58c20c1deb614b7322e6b47ab6862235e5744e
mkumbobeaty <charsbeaty@gmail.com>
2020-01-07 01:13:58 +0300

    feat: improve homepage

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <section class="home-slider"> </section>
 <section class="home-latest">
-    <div class="col-8 ">
+    <div class="col-8">
         <div class="latest-projects bg-blue">
             <h3>Latest Projects</h3>
         </div>
@@ -35,9 +35,143 @@
                 <span id="date">Dec 2, 2019</span>
                 <h4>Results for Tender bids are now available</h4>
                 <hr>
-            </div><div>
+            </div>
+            <div>
                 <span id="date">Dec 2, 2019</span>
                 <h4>Results for Tender bids are now available</h4>
+                <hr>
+            </div>
+            <div>
+                <span id="date">Dec 2, 2019</span>
+                <h4>Results for Tender bids are now available</h4>
+                <hr>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="home-latest">
+    <div class="col-8 ">
+        <div class="latest-news bg-blue">
+            <h3>Latest News</h3>
+        </div>
+        <div class="news">
+            <div class="col-3">
+                <div class="news-box"></div>
+            </div>
+            <div class="col-9">
+                <span class="news-date">Dec 2, 2019</span>
+                <h4>Results for Tender bids are now available</h4>
+                <hr>
+            </div>
+            <div class="col-3">
+                <div class="news-box"></div>
+            </div>
+            <div class="col-9">
+                <span class="news-date">Dec 2, 2019</span>
+                <h4>Results for Tender bids are now available</h4>
+                <hr>
+            </div>
+            <div class="col-3">
+                <div class="news-box"></div>
+            </div>
+            <div class="col-9">
+                <span class="news-date">Dec 2, 2019</span>
+                <h4>Results for Tender bids are now available</h4>
+                <hr>
+            </div>
+        </div>
+    </div>
+    <div class="col-4 ">
+        <div class="latest-events bg-blue">
+            <h3>Events</h3>
+        </div>
+        <div class="events">
+            <div>
+                <span id="date">Dec 2, 2019</span>
+                <h4>Launching Care for Growth - Zanzibar Serena Hotel.</h4>
+                <hr>
+            </div>
+            <div>
+                <span id="date">Dec 2, 2019</span>
+                <h4>Launching Care for Growth - Zanzibar Serena Hotel.</h4>
+                <hr>
+            </div><div>
+                <span id="date">Dec 2, 2019</span>
+                <h4>Launching Care for Growth - Zanzibar Serena Hotel.</h4>
+                <hr>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="home-docs">
+<div class="col-4 ">
+        <div class="latest-docs bg-blue">
+            <h3>STANDARD BIDDING DOCS</h3>
+        </div>
+        <div class="docs">
+            <div>
+                <h4>Standard for Leasing of Public Assets</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>STD for Procurement of Non-Consultant Services</h4>
+                <hr>
+            </div><div>
+                <h4>STD for Procurement of Non-Consultant Services</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Standard Prequalification Document</h4>
+                <hr>
+            </div>
+        </div>
+    </div>
+    <div class="col-4 ">
+        <div class="latest-docs bg-blue">
+            <h3>GUIDELINES</h3>
+        </div>
+        <div class="docs">
+            <div>
+                <h4>Procurement of Conference Services from Public Bodies</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Procurement of Capital Equipment, Materials, Products and Related</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Services for Development of Industries</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Determination of Major and Minor Deviation</h4>
+                <hr>
+            </div>
+        </div>
+    </div>
+    <div class="col-4 ">
+        <div class="latest-docs bg-blue">
+            <h3>RELATED LINKS</h3>
+        </div>
+        <div class="docs">
+            <div>
+                <h4>Ministry of Finance and Planning</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Ministry of Finance and Planning</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Bank of Tanzania</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>Tanzania Private Sector Association</h4>
+                <hr>
+            </div>
+            <div>
+                <h4>The World Bank</h4>
                 <hr>
             </div>
         </div>
