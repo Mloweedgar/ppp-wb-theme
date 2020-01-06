739368d346f1190706a37d9bd29bfd37edec2c3e
mloweedgar <mloweedgar0@gmail.com>
2020-01-06 17:16:13 +0300

    feat: setup footer structure

--- footer.php
+++ footer.php
@@ -1,4 +1,20 @@
-<footer>this is footer section </footer>
+<footer class="site-footer">
+    <section class="main-footer">
+       <section class="subscription-section">
+           this is subscription section
+       </section>
+        <section class="quicklinks">
+            This is quicklinks section
+        </section>
+        <section class="site-visits">
+            this is site visitors section
+        </section>
+
+    </section>
+    <section class="footer-navigation">
+        this is footer-navigation
+    </section>
+</footer>
 </div>
 <?php wp_footer(); ?>
 </body>
