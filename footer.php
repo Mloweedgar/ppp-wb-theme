6f7ca7a59a2468abce99be0f762b2f1962271530
mloweedgar <mloweedgar0@gmail.com>
2020-01-06 18:49:16 +0300

    feat: setup footer for the site

--- footer.php
+++ footer.php
@@ -1,18 +1,41 @@
 <footer class="site-footer">
-    <section class="main-footer">
+    <section class="main-footer black-background white-text-color">
        <section class="subscription-section">
-           this is subscription section
+           <h4>SUBSCRIBE TO NEWSLETTER & UPDATES</h4>
+           <form>
+               <input type="email"/>
+               <input type="submit" value="SUBSCRIBE"/>
+           </form>
        </section>
-        <section class="quicklinks">
-            This is quicklinks section
+        <section class="quickcontacts">
+            <h4> QUICK CONTACTS</h4>
+            <address>
+                Kijitonyama Dar es salaam <br/>
+                SLP 16 Dar es salaam <br/>
+                +255 716 645 453 <br/>
+                +255 789 673 673 <br/>
+                info@pppnode.go.tz
+            </address>
         </section>
         <section class="site-visits">
-            this is site visitors section
+            <h4>SITE VISITORS</h4>
+            <div>
+                online: 5 <br/>
+                Today: 26 <br/>
+                Yesterday: 65 <br/>
+                Week: 320 <br/>
+                Month: 1302 <br/>
+                Year: 159663 <br/>
+            </div>
         </section>
 
     </section>
-    <section class="footer-navigation">
-        this is footer-navigation
+    <section class="footer-navigation dark-grey-background white-text-color">
+        <nav>
+            <a>StaffMail</a>
+            <a>Disclaimer</a>
+            <a>Contact Us</a>
+        </nav>
     </section>
 </footer>
 </div>
