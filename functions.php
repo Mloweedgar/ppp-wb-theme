25945483e98ebb5c0fcfe1fb42e5b0c83b2b7d96
mkumbobeaty <charsbeaty@gmail.com>
2020-01-07 18:09:33 +0300

    feat: implemented about us page

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function ppp_scripts() {
     wp_enqueue_style( 'common-styles', get_template_directory_uri() . '/css/common-styles.css' );
     wp_enqueue_style( 'header', get_template_directory_uri() . '/css/header.css' );
     wp_enqueue_style( 'home', get_template_directory_uri() . '/css/home.css' );
+    wp_enqueue_style( 'about-styles', get_template_directory_uri() . '/css/about.css');
     wp_enqueue_style( 'footer', get_template_directory_uri() . '/css/footer.css' );
     wp_enqueue_style( 'sidebar', get_template_directory_uri() . '/css/sidebar.css' );
 
