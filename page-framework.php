26a90e4920d861e3ed3bab91e7474eb3bbf17cad
mloweedgar <mloweedgar0@gmail.com>
2020-01-08 15:09:15 +0300

    feat: implement framework page

--- page-framework.php
+++ page-framework.php
@@ -16,6 +16,60 @@
                     framework refers to specific PPP law and regulations that govern PPP project lifecycle, as well as
                     other legislation that affect PPP contracts, decision processes and implementation procedures.</p>
             </article>
+            <section class="">
+                <article>
+                    <h4>PPP LAWS AND REGULATIONS</h4>
+                    <ul>
+                        <li><a>2010 PPP Act</a></li>
+                        <li><a>2018 PPP Act</a></li>
+                        <li><a>PPP Regulations 2015</a></li>
+                        <li><a>PPP Guidelines</a></li>
+                    </ul>
+                    <h4>PUBLIC FINANCIAL MANAGEMENT LAWS</h4>
+                    <ul>
+                        <li><a>2019 Finance Act</a></li>
+                    </ul>
+                    <h4>SECTOR LAWS AND REGULATORY FRAMEWORKS</h4>
+                    <p>Each sector has its own laws and regulatory frameworks, please visit the ministry websites and
+                        offices of the sector you are interested in for more details</p>
+                </article>
+                <article>
+                    <h4>OTHER LAWS AFFECT CONTRACTS AND OPERATION OF PRIVATE FIRMS</h4>
+                    <ul>
+                        <li><a>Environmental Management Laws and Regulations</a></li>
+                        <li><a>Land Laws and Regulations</a></li>
+                        <li><a>Contract Laws</a></li>
+                        <li><a>PPP Guidelines</a></li>
+                    </ul>
+                </article>
+            </section>
+        </section>
+        <section class="ppp-public-communications-strategy">
+            <article>
+                <h2 class="bluish-text-color">PPP Public Communications Strategy</h2>
+                <p>The PPP Department has developed a communications strategy comprising of a framework and
+                    guidelines for public communications for PPP projects in Zanzibar. The overall goal of the
+                    strategy is to create a system that will ensure that identification, procurement and management
+                    of PPP contracts in Zanzibar is well communicated to the public. A number of laws were reviewed
+                    in the development of the framework and underlying guidelines.</p>
+                <article>
+                    <h5>The PPP public communications document aims to achieve the following:</h5>
+                    <ul>
+                        <li>Design and implement clear and concise guidance for systematic communication of PPP projects and contract information.</li>
+                        <li>Ensure clarity of information to be communicated and set timelines for such communications.</li>
+                        <li>Evaluate information on actual performance against agreed levels</li>
+                        <li>Assess information prior to disclosure.</li>
+                    </ul>
+                </article>
+                <article>
+                    <ul class="isolated-list">
+                        <li>PPP Project Information Portal</li>
+                        <li>PPP Public Communications Strategy</li>
+                        <li>Evaluate information on actual performance against agreed levels</li>
+                        <li>Guidance on Communications in relations to PPP contracts</li>
+                    </ul>
+                </article>
+            </article>
         </section>
     </div>
 <?php get_footer(); ?>
