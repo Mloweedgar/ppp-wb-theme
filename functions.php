b7fead81a5c5a44f45990518d84c3b156831fddf
mloweedgar <mloweedgar0@gmail.com>
2020-01-08 12:31:18 +0300

    feat: implement dark image section

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function ppp_scripts() {
     wp_enqueue_style( 'common-styles', get_template_directory_uri() . '/css/common-styles.css' );
     wp_enqueue_style( 'header', get_template_directory_uri() . '/css/header.css' );
     wp_enqueue_style( 'home', get_template_directory_uri() . '/css/home.css' );
+    wp_enqueue_style( 'framework', get_template_directory_uri() . '/css/framework.css' );
     wp_enqueue_style( 'footer', get_template_directory_uri() . '/css/footer.css' );
     wp_enqueue_style( 'sidebar', get_template_directory_uri() . '/css/sidebar.css' );
 
---
+++ page-framework.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+    <div class="page-framework">
+        <section class="page-intro-section">
+            <article class=" white-text-color">
+                <h1>PPP FRAMEWORK</h1>
+                <p>Promoting partnerships between the government, private sector and development partners in order to
+                    foster
+                    efficient and quality delivery of social goods and services
+                </p>
+            </article>
+        </section>
+        <section class="ppp-legal-framework">
+            <article>
+                <h2 class="bluish-text-color">PPP Legal Framework</h2>
+                <p>The PPP legal rights and obligations for both Contracting Authority and Private Party. The PPP legal
+                    framework refers to specific PPP law and regulations that govern PPP project lifecycle, as well as
+                    other legislation that affect PPP contracts, decision processes and implementation procedures.</p>
+            </article>
+        </section>
+    </div>
+<?php get_footer(); ?>
