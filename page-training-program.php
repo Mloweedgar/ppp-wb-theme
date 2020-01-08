93db8d9c53596f7105372aed913abe8326c86164
mkumbobeaty <charsbeaty@gmail.com>
2020-01-09 02:57:44 +0300

    feat:implement training page

--- page-training-program.php
+++ page-training-program.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
   <section class="program bg-blue">
-            <article class="white-text-color">
+            <div class="white-text-color">
                 <h2>ppp TRAINING PROGRAM</h2>
                 <p>
                   In order to avoid wastage of resources and public 
@@ -8,10 +8,10 @@
                   the PPP process is well planned and executed. According 
                   to best practices, the approach should be planned into multiple phases
                 </p>
-            </article>
+            </div>
   </section>
   <section class="layout-content training-program">
-  <div class="col-4 ">
+  <div class="training col-4">
         <div class="training-module">
             <h4>PPP TRAINING MODULES</h4>
         </div>
@@ -37,17 +37,78 @@
         </div>
     </div>
     <div class="col-8">
-            <h3>Overview</h3>
-            <p>Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services.</p>
-            <div class="overview">
-                <h4>Providing financial services to private sector through governments</h4>
-                <p>Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services.</p>
+      <div>
+            <h3 id="training-title">PPP Training Overview</h3>
+            <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. In order to avoid wastage 
+               of resources and public opportunities, care has to be exercised
+                in ensuring that the PPP process is well planned and executed.
+                 According to best practices, the approach should be planned 
+                 into multiple phases. </p>
+                 <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. In order to avoid wastage 
+               of resources and public opportunities, care has to be exercised
+                in ensuring that the PPP process is well planned and executed.
+                 According to best practices, the approach should be planned 
+                 into multiple phases. </p>
+      </div>
+      <div>
+            <h3 id="training-title">Curriculum / Modules</h3>
+            <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. In order to avoid wastage 
+               of resources and public opportunities, care has to be exercised
+                in ensuring that the PPP process is well planned and executed.
+                 According to best practices, the approach should be planned 
+                 into multiple phases. </p>
+      </div>
+      <div>
+            <h3 id="training-title">Instructors</h3>
+            <div class="training-instructor">
+            <div class="col-4 ">
+            <!-- <img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt=""> -->
+            <div class="instructor-box"></div>
+                <h5>Mr. Instructor</h5>
+                <p>Associate Professor, Innovation Dpt, UDSM</p>
             </div>
-            <div class="overview">
-                 <h4>Promoting knowledge sharing through private sector</h4>
-                <p>Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services.</p>
-                <p>Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services. Promoting partnerships between the government, private sector and development partners in order to foster efficient and quality delivery of social goods and services.</p>
+            <div class="col-4">
+            <!-- <img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt=""> -->
+            <div class="instructor-box"></div>
+                <h5>Mr. Instructor</h5>
+                <p>Associate Professor, Innovation Dpt, UDSM</p>
             </div>
+            <div class="col-4">
+            <div class="instructor-box"></div>
+            <!-- <img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt=""> -->
+                <h5>Mr. Instructor</h5>
+                <p>Associate Professor, Innovation Dpt, UDSM</p>
+            </div>
+            </div>
+      </div>
+      <div>
+            <h3 id="training-title">Certification</h3>
+            <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. </p>
+      </div>
+      <div>
+            <h3 id="training-title">Who should take this program</h3>
+            <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. In order to avoid wastage 
+               of resources and public opportunities, care has to be exercised
+                in ensuring that the PPP process is well planned and executed.
+                 According to best practices, the approach should be planned 
+                 into multiple phases. </p>
+      </div>
+      
     </div>
 </section>
 <?php get_footer(); ?>
