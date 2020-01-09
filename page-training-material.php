914197f4ce0ca45a3a115e51d63bf5204a64a090
mkumbobeaty <charsbeaty@gmail.com>
2020-01-09 03:43:11 +0300

    feat: add training-material page

--- page-training-material.php
+++ page-training-material.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
   <section class="training bg-blue">
             <div class="white-text-color">
-                <h2>ppp TRAINING PROGRAM</h2>
+                <h2>Training materials</h2>
                 <p>
                   In order to avoid wastage of resources and public 
                   opportunities, care has to be exercised in ensuring that 
@@ -11,24 +11,21 @@
             </div>
   </section>
   <section class="layout-content training-program">
-  <div class="program col-4">
-        <div class="training-module">
-            <h4>PPP TRAINING MODULES</h4>
-        </div>
-        <div class="modules">
+  <div class="material col-4">
+        <div class="phases">
             <div>
-                <h4><a href="#">Module 1 - Project Identification and Preparation</a></h4>
+                <h4><a href="#">Phase 1 - Project Identification and Preparation</a></h4>
                 <p><a href="#">1.1 - Project Identification</a></p>
                 <p><a href="#">1.2 - Project Preparation</a></p>
             </div>
             <div>
-                <h4><a href="#">Module 2 - Project Tendering and Selection</a></h4>
+                <h4><a href="#">Phase 2 - Project Tendering and Selection</a></h4>
                 <p><a href="#">2.1 - Project Tendering Process</a></p>
                 <p><a href="#">2.2 - Bid Selection Process</a></p>
                 <p><a href="#">2.3 - Contract Award / Signature and Financial Closing</a></p>
             </div>
             <div>
-                <h4><a href="#">Module 3 - Project Operations and Monitoring</a></h4>
+                <h4><a href="#">Phase 3 - Project Operations and Monitoring</a></h4>
                 <p><a href="#">3.1 - Project Constructions</a></p>
                 <p><a href="#">3.2 - Project Operations</a></p>
                 <p><a href="#">3.3 - Project Management and Regulations</a></p>
@@ -38,7 +35,7 @@
     </div>
     <div class="col-8">
       <div>
-            <h3 id="training-title"><a href="#">PPP Training Overview<a></h3>
+            <h3 id="training-title"><a href="#">Phase 1 - Project Identification and Preparation<a></h3>
             <p>In order to avoid wastage of resources and public opportunities,
                care has to be exercised in ensuring that the PPP process is well 
                planned and executed. According to best practices, the approach 
@@ -47,6 +44,7 @@
                 in ensuring that the PPP process is well planned and executed.
                  According to best practices, the approach should be planned 
                  into multiple phases. </p>
+                 <p id="training-title"><a href="#">1.1 - Project Identification</a></p>
                  <p>In order to avoid wastage of resources and public opportunities,
                care has to be exercised in ensuring that the PPP process is well 
                planned and executed. According to best practices, the approach 
@@ -55,50 +53,14 @@
                 in ensuring that the PPP process is well planned and executed.
                  According to best practices, the approach should be planned 
                  into multiple phases. </p>
-      </div>
-      <div>
-            <h3 id="training-title"><a href="#">Curriculum / Modules</a></h3>
-            <p>In order to avoid wastage of resources and public opportunities,
-               care has to be exercised in ensuring that the PPP process is well 
-               planned and executed. According to best practices, the approach 
-               should be planned into multiple phases. In order to avoid wastage 
-               of resources and public opportunities, care has to be exercised
-                in ensuring that the PPP process is well planned and executed.
-                 According to best practices, the approach should be planned 
-                 into multiple phases. </p>
-      </div>
-      <div>
-            <h3 id="training-title"><a href="#">Instructors</a></h3>
-            <div class="training-instructor">
-            <div class="col-4 ">
-            <!-- <img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt=""> -->
-            <div class="instructor-box"></div>
-                <h5>Mr. Instructor</h5>
-                <p>Associate Professor, Innovation Dpt, UDSM</p>
-            </div>
-            <div class="col-4">
-            <!-- <img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt=""> -->
-            <div class="instructor-box"></div>
-                <h5>Mr. Instructor</h5>
-                <p>Associate Professor, Innovation Dpt, UDSM</p>
-            </div>
-            <div class="col-4">
-            <div class="instructor-box"></div>
-            <!-- <img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt=""> -->
-                <h5>Mr. Instructor</h5>
-                <p>Associate Professor, Innovation Dpt, UDSM</p>
-            </div>
-            </div>
-      </div>
-      <div>
-            <h3 id="training-title"><a href="#">Certification</a></h3>
-            <p>In order to avoid wastage of resources and public opportunities,
+                 <p  id="training-title"><a href="#"> 1.2 - Project Preparation</a></p>
+                 <p>In order to avoid wastage of resources and public opportunities,
                care has to be exercised in ensuring that the PPP process is well 
                planned and executed. According to best practices, the approach 
                should be planned into multiple phases. </p>
       </div>
       <div>
-            <h3 id="training-title"><a href="#">Who should take this program</a></h3>
+            <h3 id="training-title"><a href="#">Phase 2 - Project Tendering and Selection<a></h3>
             <p>In order to avoid wastage of resources and public opportunities,
                care has to be exercised in ensuring that the PPP process is well 
                planned and executed. According to best practices, the approach 
@@ -107,6 +69,25 @@
                 in ensuring that the PPP process is well planned and executed.
                  According to best practices, the approach should be planned 
                  into multiple phases. </p>
+                 <p id="training-title"><a href="#">2.1 - Project Tendering Process</a></p>
+                 <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. In order to avoid wastage 
+               of resources and public opportunities, care has to be exercised
+                in ensuring that the PPP process is well planned and executed.
+                 According to best practices, the approach should be planned 
+                 into multiple phases. </p>
+                 <p  id="training-title"><a href="#"> 2.2 - Bid Selection Process</a></p>
+                 <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. </p>
+               <p  id="training-title"><a href="#"> 2.3 - Contract Award / Signature and Financial Closing</a></p>
+                 <p>In order to avoid wastage of resources and public opportunities,
+               care has to be exercised in ensuring that the PPP process is well 
+               planned and executed. According to best practices, the approach 
+               should be planned into multiple phases. </p>
       </div>
       
     </div>
