05635a66695a8bdf559726b1b8f1be5517512a09
mkumbobeaty <charsbeaty@gmail.com>
2020-01-12 21:28:20 +0300

    feat: added faqs page

---
+++ page-faq.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+<section>
+    <div class="bluish-text-color greysh-background">
+    <h2 id="faq_title">Faqs</h2>
+</div>
+<div class="faqs bluish-text-color">
+    <h4>What is PPP?</h4>
+    <hr>
+    <h4>What are the objectives of PPP?</h4>
+    <hr> 
+    <h4>How can I take part in PPP?</h4>
+    <hr>
+    <h4>Where can I find PPP regulations?</h4>
+    <hr>
+    <h4>How are PPP Trainings organized</h4>
+    <hr>
+    <h4>How are PPP Trainings organized</h4>
+    <hr>
+    <h4>Where can I get trained PPP</h4>
+    <hr>
+</div>
+</section>
+<?php get_footer(); ?>
