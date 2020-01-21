c68df5af202b7a97eef9ca7e067f41c556b809ab
mloweedgar <mloweedgar0@gmail.com>
2020-01-21 11:28:26 +0300

    feat: implement page.php and single.php

--- content-events.php
+++ content-events.php
@@ -1,5 +1,5 @@
 <div>
     <span id="date"><?php echo get_the_date(); ?></span>
-    <h4><?php the_title(); ?></h4>
+    <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
     <hr>
 </div>
--- content-projects.php
+++ content-projects.php
@@ -2,7 +2,7 @@
 <div class="col-6">
     <hgroup>
         <h4><?php the_category( ', ' ); ?></h4>
-        <h2><?php the_title(); ?></h2>
+        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     </hgroup>
     <p><?php the_excerpt(); ?></p>
 </div>
