abbedf03c895987cf3966a1f30e4e5f57525611d
mkumbobeaty <charsbeaty@gmail.com>
2020-01-23 15:04:37 +0300

    initialize responsiveness

--- header.php
+++ header.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
+<html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Zanzibar PPP department</title>
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
@@ -23,14 +25,14 @@
                 </select>
             </aside>
         </section>
-        <section class="logos-section">
+        <!-- <section class="logos-section">
             <img src="<?php echo get_bloginfo('template_directory'); ?>/images/zanzibar-coat-of-arms.jpg"
                  alt="Zanzibar Coat of Arms" width="150" height="100">
             <h3 class="bluish-text-color">Zanzibar Private Public Partnership <br/>(PPP) Department</h3>
             <img src="<?php echo get_bloginfo('template_directory'); ?>/images/flag-of-zanzibar.svg"
                  alt="Flat of Zanzibar" width="130" height="130">
 
-        </section>
+        </section> -->
         <section class="main-navigation">
             <nav class="bluish-text-color">
                 <?php
@@ -38,9 +40,9 @@
                 ?>
             </nav>
 <!--            --><?php //get_search_form( ); ?>
-            <div class="search-section">
+            <!-- <div class="search-section">
                 <input type="search"/>
                 <button><img src="<?php echo get_bloginfo('template_directory'); ?>/images/icons-search.svg"/></button>
-            </div>
+            </div> -->
         </section>
     </header>
