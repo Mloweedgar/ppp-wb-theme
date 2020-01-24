68b3c69a1b50379e9f92f92f6bdcc296fafb7868
mloweedgar <mloweedgar0@gmail.com>
2020-01-24 16:39:58 +0300

    feat: implement navigation bar for mobile

--- includes/header-for-mobile.php
+++ includes/header-for-mobile.php
@@ -1,3 +1,22 @@
 <header class="navigation-bar-mobile">
-    this is  header for mobile screen
+    <img src="<?php echo get_bloginfo('template_directory'); ?>/images/zanzibar-coat-of-arms.jpg"
+         alt="Zanzibar Coat of Arms" width="60" height="40">
+    <section class="lef-nav-section">
+        <div class="language-selector-contents-mobile">
+            <select class="language-selector">
+                <option selected="selected">EN</option>
+                <option>SW</option>
+            </select>
+        </div>
+        <div class="menu-icon" onclick="myFunction(this)">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+        </div>
+    </section>
 </header>
+<script>
+    function myFunction(x) {
+        x.classList.toggle("change");
+    }
+</script>
