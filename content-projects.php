fc3bb28ea5098fe27579450c830748b609ebe343
mloweedgar <mloweedgar0@gmail.com>
2020-01-25 19:21:48 +0300

    feat: make news section response

--- content-projects.php
+++ content-projects.php
@@ -4,5 +4,5 @@
         <h4><?php the_category( ', ' ); ?></h4>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     </hgroup>
-    <p><?php the_excerpt(); ?></p>
+    <?php the_excerpt(); ?>
 </div>
