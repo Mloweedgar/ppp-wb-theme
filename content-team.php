1d4ab21031fcd88a2eb59e798b4490f58aadfae8
mloweedgar <mloweedgar0@gmail.com>
2020-01-27 15:40:45 +0300

    feat: display team members well

--- content-team.php
+++ content-team.php
@@ -1,7 +1,7 @@
 <div class="col-4">
     <img src="<?php if ( has_post_thumbnail() ) {
         the_post_thumbnail_url();
-    } ?>" alt="team member">
+    } ?>" alt="team member" width="180" height="140">
     <h5><?php the_title() ?></h5>
     <p>Commissioner</p>
 </div>
