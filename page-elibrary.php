10ce573e0d33f60b9d8c4202e9025b1881d474da
mkumbobeaty <charsbeaty@gmail.com>
2020-02-11 16:08:33 +0300

    feat: implement document page and link e-library with the document page

--- page-elibrary.php
+++ page-elibrary.php
@@ -13,32 +13,32 @@
   <tr>
     <td>Description</td>
     <td>Maria Anders</td>
-    <td>2</td>
+    <td><a href="<?php echo get_home_url(); ?>/document">2</a></td>
   </tr>
   <tr>
     <td>Centro comercial Moctezuma</td>
     <td>Francisco Chang</td>
-    <td>2</td>
+    <td><a href="<?php echo get_home_url(); ?>/document">2</a></td>
   </tr>
   <tr>
     <td>Ernst Handel</td>
     <td>Roland Mendel Centro comercial Moctezuma</td>
-    <td>12</td>
+    <td><a href="<?php echo get_home_url(); ?>/document">12</a></td>
   </tr>
   <tr>
     <td>Island Trading</td>
     <td>Helen Bennett</td>
-    <td>14</td>
+    <td><a href="<?php echo get_home_url(); ?>/document">14</a></td>
   </tr>
   <tr>
     <td>Laughing Bacchus Winecellars</td>
     <td>Yoshi Tannamuri</td>
-    <td>9</td>
+    <td><a href="<?php echo get_home_url(); ?>/document">9</a></td>
   </tr>
   <tr>
     <td>Magazzini Alimentari Riuniti</td>
     <td>Giovanni Rovelli</td>
-    <td>10</td>
+    <td><a href="<?php echo get_home_url(); ?>/document">10</a></td>
   </tr>
 </table>
 </div>
