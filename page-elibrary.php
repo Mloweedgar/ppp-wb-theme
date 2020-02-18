dc2959daf7c074fc2fe30bc61f7812c9c81259e9
mloweedgar <mloweedgar0@gmail.com>
2020-02-18 16:07:45 +0300

    feat(e-library): load categories dynamically

--- page-elibrary.php
+++ page-elibrary.php
@@ -1,46 +1,33 @@
 <?php get_header(); ?>
     <section>
-    <div class="bluish-text-color">
-        <h2 id="news"> E- library</h2>
-    </div>
-    <div class="e_library">
-    <table>
-  <tr class="table-header">
-    <th>Category</th>
-    <th>Description</th>
-    <th>Number of Documents</th>
-  </tr>
-  <tr>
-    <td>Description</td>
-    <td>Maria Anders</td>
-    <td><a href="<?php echo get_home_url(); ?>/document">2</a></td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td><a href="<?php echo get_home_url(); ?>/document">2</a></td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel Centro comercial Moctezuma</td>
-    <td><a href="<?php echo get_home_url(); ?>/document">12</a></td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td><a href="<?php echo get_home_url(); ?>/document">14</a></td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td><a href="<?php echo get_home_url(); ?>/document">9</a></td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td><a href="<?php echo get_home_url(); ?>/document">10</a></td>
-  </tr>
-</table>
-</div>
+        <div class="bluish-text-color">
+            <h2 id="news"> E- library</h2>
+        </div>
+        <div class="e_library">
+            <table>
+                <tr class="table-header">
+                    <th>Category</th>
+                    <th>Description</th>
+                    <th>Number of Documents</th>
+                </tr>
+                <?php
+                $categories = get_categories(array(
+                    'orderby' => 'name',
+                    'order' => 'ASC'
+                ));
+
+                foreach ($categories as $category) {
+                    ?>
+                    <tr>
+                        <td><?php echo $category->name ?></td>
+                        <td><?php echo $category->description ?></td>
+                        <td>
+                            <a href="<?php echo get_category_link($category->term_id) ?>"><?php echo $category->count ?></a>
+                        </td>
+                    </tr>
+
+                <?php } ?>
+            </table>
+        </div>
     </section>
 <?php get_footer(); ?>
