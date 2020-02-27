74074f3a5b594674d277fd6cc10b33bf11fa66c0
mloweedgar <mloweedgar0@gmail.com>
2020-02-27 21:07:48 +0300

    feat: remove english swahili select

--- includes/header-for-mobile.php
+++ includes/header-for-mobile.php
@@ -2,12 +2,6 @@
     <img src="<?php echo get_bloginfo('template_directory'); ?>/images/zanzibar-coat-of-arms.jpg"
          alt="Zanzibar Coat of Arms" width="60" height="40">
     <section class="lef-nav-section">
-        <div class="language-selector-contents-mobile">
-            <select class="language-selector">
-                <option selected="selected">EN</option>
-                <option>SW</option>
-            </select>
-        </div>
         <div class="menu-icon" onclick="showMenu()">
             <div class="bar1"></div>
             <div class="bar2"></div>
