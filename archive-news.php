b535df6ccdf25881a9aba16e46925d645076dacf
mloweedgar <mloweedgar0@gmail.com>
2020-03-06 18:58:05 +0300

    feat: load news data dynamically

--- archive-news.php
+++ archive-news.php
@@ -3,103 +3,33 @@
     <div class="bluish-text-color">
         <h2 id="news">news</h2>
     </div>
-    <div  class="layout-content">
-        <div class="col-6 news_section">
-            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/home-slider.jpeg" alt="">
-            <p class="news-date">Dec 2, 2019</p>
-            <h4>Zanzibar updates its poverty reduction strategy ahead of the general election</h4>
-        </div>
-        <div class="col-6 news_section">
-            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/home-slider.png" alt="">
-            <p class="news-date">Nov 24, 2019</p>
-            <h4>A new partnership that will provide tremendous support to fishing community in Zanzibar </h4>
-        </div>
-        <div class="col-6 news_section">
-            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/home-slider.jpeg" alt="">
-            <p class="news-date">Dec 2, 2019</p>
-            <h4>Zanzibar updates its poverty reduction strategy ahead of the general election</h4>
-        </div>
-        <div class="col-6 news_section">
-            <img src="<?php echo get_bloginfo('template_directory'); ?>/images/home-slider.png" alt="">
-            <p class="news-date">Nov 24, 2019</p>
-            <h4>A new partnership that will provide tremendous support to fishing community in Zanzibar </h4>
-        </div>
+    <div class="layout-content">
+        <?php
+        $ids = array(); // ids of first 4 latest posts
+        $args = array(
+            'post_type' => 'news',
+            'posts_per_page' => 4,
+        );
+        $custom_query = new WP_Query($args);
+        while ($custom_query->have_posts()) : $custom_query->the_post();
+            $ids[] = get_the_ID();
+            get_template_part('content-news-card', get_post_format());
+        endwhile; ?>
     </div>
 </section>
 <section class="layout-content older_news_section">
     <h2 id="news_header">Older</h2>
-    <div  class="layout-content oldnews">
-        <div class="col-6 news_section">
-            <div class="layout-content older_news">
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-            </div>
-        </div>
-        <div class="col-6 news_section">
-            <div class="layout-content older_news">
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-                <div class="col-3">
-                    <div class="news-box"></div>
-                </div>
-                <div class="col-9">
-                    <span class="news-date">Dec 2, 2019</span>
-                    <h4>Results for Tender bids are now available</h4>
-                    <hr>
-                </div>
-            </div>
+    <div class="layout-content oldnews">
+        <div class="older-section">
+            <?php
+            $args = array(
+                'post_type' => 'news',
+                'post__not_in' => $ids
+            );
+            $custom_query = new WP_Query($args);
+            while ($custom_query->have_posts()) : $custom_query->the_post();
+                get_template_part('content-news-card-small', get_post_format());
+            endwhile; ?>
         </div>
     </div>
 </section>
