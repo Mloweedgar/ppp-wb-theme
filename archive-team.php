744ddf3ab22773a3a7d34f2b5411d6c6c4c4735f
mloweedgar <mloweedgar0@gmail.com>
2020-03-10 08:37:16 +0300

    feat: add team post type

--- archive-team.php
+++ archive-team.php
@@ -2,40 +2,58 @@
     <section class="layout-content organization">
         <div class="col-8">
             <div class="org-structure">
-                <h3>ZANZIBAR PPP DEPARTMENT SECRETARIAT</h3>
-                <div class="org-department commissioner">
+                <h3>MEET THE TEAM</h3>
+                <div class="organisation-structure-level-one the-team">
                     <?php
                     $args = array(
                         'post_type' => 'team',
-                        'posts_per_page' => 2,
+                        'meta_query'	=> array(
+                            array(
+                                'key'	  	=> 'ppp-organisation-structure-level',
+                                'value'	  	=> 1,
+                                'compare' 	=> '=',
+                            ),
+                        ),
+                        'posts_per_page' => -1,
                     );
                     $custom_query = new WP_Query($args);
                     while ($custom_query->have_posts()) : $custom_query->the_post();
                         get_template_part('content-team', get_post_format());
                     endwhile; ?>
                 </div>
+                <div class="the-team">
+                    <?php
+                    $args = array(
+                        'post_type' => 'team',
+                        'meta_query'	=> array(
+                            array(
+                                'key'	  	=> 'ppp-organisation-structure-level',
+                                'value'	  	=> 2,
+                                'compare' 	=> '=',
+                            ),
+                        ),
+                        'posts_per_page' => -1,
+                    );
+                    $custom_query = new WP_Query($args);
+                    while ($custom_query->have_posts()) : $custom_query->the_post();
+                        get_template_part('content-team', get_post_format());
+                    endwhile; ?>
+                </div>
+
                 <div class="org-department">
                     <?php
-
                     $args = array(
                         'post_type' => 'team',
-                        'posts_per_page' => 4,
-                    );
-                    $query_1 = new WP_Query( $args );
-
-                    $exclude_posts = array();
-
-                    while ( $query_1->have_posts() ) {
-                        $query_1->the_post();
-                        $exclude_posts[] = get_the_ID();
-                    }
-
-                    $args = array(
+                        'meta_query'	=> array(
+                            array(
+                                'key'	  	=> 'ppp-organisation-structure-level',
+                                'value'	  	=> 3,
+                                'compare' 	=> '=',
+                            ),
+                        ),
                         'posts_per_page' => -1,
-                        'post_type' => 'team',
-                        'post__not_in' => $exclude_posts
                     );
-                    $custom_query = new WP_Query( $args );
+                    $custom_query = new WP_Query($args);
                     while ($custom_query->have_posts()) : $custom_query->the_post();
                         get_template_part('content-team', get_post_format());
                     endwhile; ?>
--- content-team.php
+++ content-team.php
@@ -1,9 +1,10 @@
-<div class="col-4">
-<img src="<?php echo get_bloginfo('template_directory'); ?>/images/team.png" alt="">
-
-    <!-- <img src="<?php if ( has_post_thumbnail() ) {
+<div class="team-member">
+    <img src="<?php if ( has_post_thumbnail() ) {
         the_post_thumbnail_url();
-    } ?>" alt="team member"> -->
-    <h5><?php the_title() ?></h5>
-    <p><?php the_content() ?></p>
+    } ?>" alt="team member" height="180"  width="180">
+    <div class="team-member-contents">
+        <h4><?php the_title() ?></h4>
+        <div><?php echo get_post_meta(get_the_ID(), 'ppp-team-member-title', true); ?></div>
+        <p><?php the_content() ?></p>
+    </div>
 </div>
