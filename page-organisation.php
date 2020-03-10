d1c33f0ea1036b48116232b47001942a1c3198ae
mloweedgar <mloweedgar0@gmail.com>
2020-03-10 09:01:29 +0300

    feat: add image for organisation structure

--- page-organisation.php
+++ page-organisation.php
@@ -1,46 +1,7 @@
 <?php get_header(); ?>
     <section class="layout-content organization">
         <div class="col-8">
-            <div class="org-structure">
-                <h3>ZANZIBAR PPP DEPARTMENT SECRETARIAT</h3>
-                <div class="org-department commissioner">
-                    <?php
-                    $args = array(
-                        'post_type' => 'team',
-                        'posts_per_page' => 2,
-                    );
-                    $custom_query = new WP_Query($args);
-                    while ($custom_query->have_posts()) : $custom_query->the_post();
-                        get_template_part('content-team', get_post_format());
-                    endwhile; ?>
-                </div>
-                <div class="org-department">
-                    <?php
-
-                    $args = array(
-                        'post_type' => 'team',
-                        'posts_per_page' => 4,
-                    );
-                    $query_1 = new WP_Query( $args );
-
-                    $exclude_posts = array();
-
-                    while ( $query_1->have_posts() ) {
-                        $query_1->the_post();
-                        $exclude_posts[] = get_the_ID();
-                    }
-
-                    $args = array(
-                        'posts_per_page' => -1,
-                        'post_type' => 'team',
-                        'post__not_in' => $exclude_posts
-                    );
-                    $custom_query = new WP_Query( $args );
-                    while ($custom_query->have_posts()) : $custom_query->the_post();
-                        get_template_part('content-team', get_post_format());
-                    endwhile; ?>
-                </div>
-            </div>
+           <img src="<?php echo get_bloginfo('template_directory'); ?>/images/organisation-structure.gif">
         </div>
         <div class="col-4">
             <div class="related-link">
