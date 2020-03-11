87e1c25a9b0252b411c56d738545ff8dc7704cc4
mloweedgar <mloweedgar0@gmail.com>
2020-03-11 13:50:31 +0300

    feat: hide project categories on home page

--- content-projects.php
+++ content-projects.php
@@ -1,7 +1,6 @@
 
 <div class="col-6">
     <hgroup>
-        <h4><?php the_category( ', ' ); ?></h4>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     </hgroup>
     <?php the_excerpt(); ?>
