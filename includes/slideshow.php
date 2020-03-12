e7fbecd27185dbb42457a3781e1e2d349ad1bb7b
mloweedgar <mloweedgar0@gmail.com>
2020-03-12 15:15:36 +0300

    feat: modify home slider to resemble the  designs

--- includes/slideshow.php
+++ includes/slideshow.php
@@ -1,38 +1,94 @@
 <!-- Slideshow container -->
 <div class="slideshow-container">
 
-    <!-- Full-width images with number and caption text -->
     <div class="mySlides fade">
         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/3I8A0047.JPG">
+        <article class="text">
+            <p class="caption-text">
+                The Revolutionary Government of Zanzibar (RGoZ) recognizes importance of PPPs in meeting an
+                ever-increasing demand for quality public infrastructure and services. The role of the private sector,
+                particularly its technical expertise cannot be underscored, given the environmental and spatial
+                development challenges of the isles. The need to finance and develop infrastructure continues to grow
+                exponentially hence outgrows the demand for public services and put pressure on government budget. The
+                government of Zanzibar therefore has in place a robust PPP policy framework which supports competitive
+                and efficient delivery of services and creates a more conducive environment for PPP projects
+            </p>
+            <p>- Statement of the Minister -</p>
+
+        </article>
     </div>
 
     <div class="mySlides fade">
         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/DSC_1177.JPG">
+        <article class="text">
+            <p class="caption-text">
+                The Ministry of Finance works closely with the PPP Department, to coordinate preparation and procurement
+                of PPP projects, to ensure a robust PPP project pipeline. Ongoing advice and support to the PPP
+                Department facilitates integration of PPPs in the national budgets with clear allocation of risks and
+                responsibilities between the public and the private sector parties to the PPP agreement. By going so,
+                the RGoZ aspires to make transformative growth by addressing constraints and challenges for timely
+                provision quality infrastructure and services through PPPs, to accelerate social development of
+                Zanzibar.
+            </p>
+            <p>- Statement of Principal Secretary Finance -</p>
+
+        </article>
     </div>
 
     <div class="mySlides fade">
         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/DSC_1177.JPG">
+        <article class="text">
+            <p class="caption-text">
+                PPP arrangements are consistent with Zanzibar Development Vision which is the country’s long term plan
+                for social and economic development. The vision objectives are to transform Zanzibar into a
+                middle-income country and enable it to eradicate absolute poverty in society through building a strong
+                and competitive economy, achieve high quality livelihood for citizens and improve governance and the
+                rule of law without compromising Zanzibar’s rich culture. Through PPPs, the RGoZ is determined to reach
+                its development objectives set under the Zanzibar Strategy for Growth and Reduction of Poverty III.
+            </p>
+            <p>- Statement of Executive Secretary Zanzibar Planning Commission -</p>
+
+        </article>
     </div>
 
     <div class="mySlides fade">
         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/3I8A0060.JPG">
+        <article class="text">
+            <p class="caption-text">
+                The Revolutionary Government of Zanzibar (RGoZ) recognizes importance of PPPs in meeting an
+                ever-increasing demand for quality public infrastructure and services. The role of the private sector,
+                particularly its technical expertise cannot be underscored, given the environmental and spatial
+                development challenges of the isles. The need to finance and develop infrastructure continues to grow
+                exponentially hence outgrows the demand for public services and put pressure on government budget. The
+                government of Zanzibar therefore has in place a robust PPP policy framework which supports competitive
+                and efficient delivery of services and creates a more conducive environment for PPP projects
+            </p>
+            <p>- Statement of the Minister -</p>
+
+        </article>
     </div>
 
     <div class="mySlides fade">
         <img src="<?php echo get_bloginfo('template_directory'); ?>/images/3I8A0047.JPG">
+        <article class="text">
+            <p class="caption-text">
+                A multi-disciplinary team of well-trained Zanzibaris supports coordination of plans and procurements of
+                PPP projects, by offering technical advice and implementation support to the Implementing Authorities.
+                Through the capacity building program on different technical and safeguards aspects, the team is able to
+                grow skills and competencies of the Implementing Authorities for preparing and procuring viable PPP
+                projects. Our day-to-day coordination of PPP processes ensures compliance to the procedures and
+                processes set under the PPP legal and regulatory framework.
+            </p>
+            <p>- Statement of Commissioner of PPP -</p>
+
+        </article>
     </div>
 
 
     <!-- Next and previous buttons -->
     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
     <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    <!-- The dots/circles -->
-    <div class="dot-controls">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-        <span class="dot" onclick="currentSlide(4)"></span>
-        <span class="dot" onclick="currentSlide(5)"></span>
-    </div>
 </div>
+
+
 
