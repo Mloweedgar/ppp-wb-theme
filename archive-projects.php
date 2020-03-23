0cbda19b05bb7d5a625625cd95dd558e878e60f4
mloweedgar <mloweedgar0@gmail.com>
2020-03-23 19:41:51 +0300

    feat: display sub sectors in projects list

--- archive-projects.php
+++ archive-projects.php
@@ -1,36 +1,14 @@
 <?php get_header(); ?>
 <section>
-    <div class="bluish-text-color">
-        <h2 id="news">PROJECTS</h2>
-    </div>
-    <div class="layout-content">
+    <div>
         <?php
         $ids = array(); // ids of first 4 latest posts
-        $args = array(
-            'post_type' => 'projects',
-            'posts_per_page' => 4,
-        );
-        $custom_query = new WP_Query($args);
-        while ($custom_query->have_posts()) : $custom_query->the_post();
-            $ids[] = get_the_ID();
-            get_template_part('content-news-card', get_post_format());
-        endwhile; ?>
-    </div>
-</section>
-<section class="layout-content older_news_section">
-    <h2 id="news_header">Older</h2>
-    <div class="layout-content oldnews">
-        <div class="older-section">
-            <?php
-            $args = array(
-                'post_type' => 'news',
-                'post__not_in' => $ids
-            );
-            $custom_query = new WP_Query($args);
-            while ($custom_query->have_posts()) : $custom_query->the_post();
-                get_template_part('content-news-card-small', get_post_format());
-            endwhile; ?>
-        </div>
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+
+            get_template_part('content-project-card', get_post_format());
+
+        endwhile; endif;
+        ?>
     </div>
 </section>
 <?php get_footer(); ?>
--- content-project-card.php
+++ content-project-card.php
@@ -3,7 +3,7 @@
         the_post_thumbnail_url();
     } ?>" alt="posts image">
     <div class="contents-section">
-        <div class="project-category"><?php the_category( ', ' ); ?></div>
+        <div class="project-category">SUB-SECTOR:<?php the_category( ', ' ); ?></div>
         <h4 class="project-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
         <p class="project-content"><?php the_excerpt(); ?></p>
     </div>
