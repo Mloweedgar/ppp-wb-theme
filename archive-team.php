2006afa8c697aaac9c6054488a627d3d4cfdef34
mloweedgar <mloweedgar0@gmail.com>
2020-03-23 18:33:45 +0300

    feat(project): display rich content for project details

--- archive-team.php
+++ archive-team.php
@@ -24,6 +24,7 @@
                 <?php
                 $args = array(
                     'post_type' => 'team',
+                    'order' => 'ASC',
                     'meta_query' => array(
                         array(
                             'key' => 'ppp-organisation-structure-level',
