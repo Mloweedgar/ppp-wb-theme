5d2441d446153bb40e9a2947d0c9269131a7a2d8
mloweedgar <mloweedgar0@gmail.com>
2020-03-23 18:35:37 +0300

    feat: display rich contents for single post types details

--- single.php
+++ single.php
@@ -1,14 +1,5 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<div class="page">
-    <section class="page-intro-section">
-        <article class=" white-text-color">
-            <h1><?php the_title() ?></h1>
-        </article>
-    </section>
-    <section style="padding: 1%">
-        <?php the_content() ?>
-    </section>
-    <?php endwhile; endif; ?>
-
-    <?php get_footer(); ?>
+    <?php the_content(); ?>
+<?php endwhile; endif; ?>
+<?php get_footer(); ?>
