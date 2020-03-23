db1e4d1d5f7ca99730cf54a3010ddee4a07f1a83
mloweedgar <mloweedgar0@gmail.com>
2020-03-23 11:49:55 +0300

    feat: make team members clickable

--- content-news.php
+++ content-news.php
@@ -1,5 +1,5 @@
 <div class="col-3">
-    <div class="news-box"></div>
+    <div class="news-box" style="background: #E6E6E6;"></div>
 </div>
 <div class="col-9">
     <span class="news-date"><?php echo get_the_date(); ?></span>
--- content-team.php
+++ content-team.php
@@ -3,7 +3,7 @@
         the_post_thumbnail_url();
     } ?>" alt="team member" height="180"  width="180">
     <div class="team-member-contents">
-        <h4><?php the_title() ?></h4>
+        <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
         <div><?php echo get_post_meta(get_the_ID(), 'ppp-team-member-title', true); ?></div>
         <p><?php the_content() ?></p>
     </div>
