03cbf37932d0ee8fef7b4d86c236489a622e78b3
mkumbobeaty <charsbeaty@gmail.com>
2020-03-29 19:27:28 +0300

    improved news page

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
     <h3><?php the_title(); ?></h3>
-   <p> <?php the_content('<p>','</p>'); ?></p>
+   <div id="paragraph"><p> <?php the_content('<p >','</p>'); ?></p></div>
 <?php endwhile; endif; ?>
 <?php get_footer(); ?>
