dd88a3712422bab06ff171e180c0d42c47316625
Navish <navish45@gmail.com>
2020-04-04 20:57:33 +0300

    add sidebar widget

--- functions.php
+++ functions.php
@@ -422,8 +422,25 @@ function image_slider_settings_page_setup()
 
 }
 
+
 add_action('admin_init', 'image_slider_settings_page_setup');
 
+function ppp_widgets_init() {
+
+	register_sidebar(
+		array(
+			'name'          => __( 'Right Sidebar', 'PPP' ),
+			'id'            => 'right-sidebar',
+			'description'   => __( 'Add widgets here to appear in right <aside>.', 'PPP' ),
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h4 class="widget-title">',
+			'after_title'   => '</h4>',
+		)
+	);
+
+}
+add_action( 'widgets_init', 'ppp_widgets_init' );
 
 
 
